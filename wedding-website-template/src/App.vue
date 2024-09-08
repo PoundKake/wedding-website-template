373e785b62d38e4689361d033a92570492141aed
PoundKake <vance.christian@gmail.com>
2024-09-08 13:45:52 -0400

    added grid layout to main app container

--- wedding-website-template/src/App.vue
+++ wedding-website-template/src/App.vue
@@ -2,6 +2,28 @@
 
 <template>
   <header></header>
+  <body>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-row>
+            <v-card> decorative image here </v-card>
+          </v-row>
+          <v-row>
+            <v-card> wedding Image Here </v-card>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col> </v-col>
+      </v-row>
+    </v-container>
+  </body>
+  <v-icon icon="home"></v-icon>
 </template>
 
-<style scoped></style>
+<style scoped>
+v-icon {
+  background-color: black;
+}
+</style>
