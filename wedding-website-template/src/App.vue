911859d902bc37524033a484822cc22536b1618a
PoundKake <vance.christian@gmail.com>
2024-09-08 17:57:54 -0400

    removed unneeded variable

--- wedding-website-template/src/App.vue
+++ wedding-website-template/src/App.vue
@@ -15,8 +15,6 @@ const items = [
     link: listItems.About.link
   }
 ]
-const text =
-  'Lorem ipsum dolor sit amet, c sectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 </script>
 
 <template>
