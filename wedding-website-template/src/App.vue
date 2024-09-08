5880c2f9c008fc82b4da03853a1e318ee82f57e4
PoundKake <vance.christian@gmail.com>
2024-09-08 17:56:29 -0400

    Splash screen
    fixing the default splash screen images and such

--- wedding-website-template/src/App.vue
+++ wedding-website-template/src/App.vue
@@ -1,5 +1,22 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import listItems from '@/scripts/tabItems'
+
+import { ref } from 'vue'
+
+const tab = ref('Home')
+
+const bgColor = '#fffbe6'
+const fgColor = '#356859'
+
+const items = [
+  {
+    name: listItems.About.title,
+    link: listItems.About.link
+  }
+]
+const text =
+  'Lorem ipsum dolor sit amet, c sectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 </script>
 
 <template>
@@ -10,20 +27,54 @@ import { RouterView } from 'vue-router'
       <v-row>
         <v-col>
           <v-row>
-            <v-card> decorative image here </v-card>
+            <v-col>
+              <v-card class="text-center justify-center" :color="bgColor" :elevation="0">
+                decorative image here
+              </v-card>
+            </v-col>
           </v-row>
           <v-row>
-            <v-card> wedding Image Here </v-card>
+            <v-col>
+              <v-card class="text-center justify-center" :color="bgColor" :elevation="0">
+                wedding Image Here
+              </v-card>
+            </v-col>
           </v-row>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
-          <router-view></router-view>
+          <v-card :color="bgColor" :elevation="0">
+            <v-card-title class="text-center justify-center py-6">
+              <h1 class="text-h2 fc">Weddin Title</h1>
+            </v-card-title>
+
+            <v-tabs v-model="tab" bg-color="transparent" :color="fgColor" grow>
+              <v-tab
+                v-for="item in items"
+                :key="item"
+                :text="item.name"
+                :value="item"
+                :to="item.link"
+              ></v-tab>
+            </v-tabs>
+
+            <v-tabs-window v-model="tab">
+              <v-tabs-window-item v-for="item in items" :key="item" :value="item">
+                <v-card color="#fffbe6" flat>
+                  <router-view></router-view>
+                </v-card>
+              </v-tabs-window-item>
+            </v-tabs-window>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
   </v-layout>
 </template>
 
-<style scoped></style>
+<style>
+#template {
+  font-family: Didot;
+}
+</style>
--- wedding-website-template/src/views/AboutView.vue
+++ wedding-website-template/src/views/AboutView.vue
@@ -1,13 +1,7 @@
 <template>
-  <div class="about">
+  <div>
     <h1>This is an about page</h1>
   </div>
 </template>
 
-<style>
-.about {
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-}
-</style>
+<style></style>
